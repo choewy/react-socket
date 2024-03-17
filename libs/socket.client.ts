007b2f3a782d20120260c687b0fdefe4788a1156
choewy <choewy32@gmail.com>
2024-03-17 17:35:02 +0900

    feature/socket client

--- libs/socket.client.ts
+++ libs/socket.client.ts
@@ -1,35 +1,25 @@
 import { useEffect } from 'react';
-import { Manager, Socket } from 'socket.io-client';
+import { Socket, io } from 'socket.io-client';
 
 import { createUrl } from './helpers';
 import { SocketClientOptions, SocketEventHandler } from './interfaces';
 import { SocketEvent } from './socket.event';
 
-export class SocketClient extends Socket {
+export class SocketClient {
   private readonly name: string;
 
+  private socket: Socket;
+
   constructor({ name, url, namespace, ...opts }: SocketClientOptions) {
-    super(new Manager(createUrl(url, namespace), opts), '', opts);
+    this.socket = io(createUrl(url, namespace), opts);
     this.name = name ?? 'default';
-  }
-
-  useConnect() {
-    useEffect(() => {
-      if (this.disconnected) {
-        this.connect();
-
-        return () => {
-          this.disconnect();
-        };
-      }
-    }, []);
   }
 
   useOnEvent<T>(event: string, handler: SocketEventHandler<T>) {
     const eventName = SocketEvent.createEventName(this.name, event);
 
     useEffect(() => {
-      this.on(event, (...payloads) => new SocketEvent(eventName, payloads).dispatch());
+      this.socket.on(event, (...payloads) => new SocketEvent(eventName, payloads).dispatch());
     }, [eventName]);
 
     useEffect(() => {
