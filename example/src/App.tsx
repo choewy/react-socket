8f548294f8b571a97ca1317d14aa34a40bb1d7b0
choewy <choewy32@gmail.com>
2024-03-17 17:01:14 +0900

    feature/add events

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,10 +6,11 @@ function App() {
   const socket = SocketClientStorage.get('default');
 
   if (socket) {
-    socket.useConnect();
     socket.useOnEvent('connect', () => console.log('connect'));
     socket.useOnEvent('disconnect', () => console.log('disconnect'));
+    socket.useOnEvent('connect_error', (e) => console.log('connect_error', e));
     socket.useOnEvent('error', () => console.log('error'));
+    socket.useConnect();
   }
 
   return <div></div>;
